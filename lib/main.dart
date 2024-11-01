deb5b9765ae7a1dc642d99266994ed25f7a8ab82
wapiiqo <muhammadwafiq1313@gmail.com>
2024-11-01 21:48:26 +0700

    add firebase auth, register and login feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:smart_shop_app/screens/auth_screen.dart';
+import 'firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smart_shop_app/screens/cart_screen.dart';
 import 'package:smart_shop_app/screens/home_screen.dart';
@@ -6,7 +9,11 @@ import 'package:smart_shop_app/screens/product_detail_screen.dart';
 import 'package:smart_shop_app/screens/product_list.dart';
 import 'package:smart_shop_app/screens/wishlist_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const SmartShopApp());
 }
 
@@ -19,8 +26,9 @@ class SmartShopApp extends StatelessWidget {
       child: MaterialApp(
         title: 'FruitSmart',
         debugShowCheckedModeBanner: false,
-        home: const HomeScreen(),
+        home: const AuthScreen(),
         routes: {
+          '/auth': (context) => const AuthScreen(),
           '/homescreen': (context) => const HomeScreen(),
           '/product-list': (context) => const ProductListScreen(),
           '/detail': (context) => const ProductDetailScreen(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,117 +4,134 @@ import 'package:google_fonts/google_fonts.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Future<String> getDataFromFirebase() async {
+    await Future.delayed(const Duration(seconds: 2));
+    return "Data Loaded";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 234, 0),
-      body: CustomScrollView(
-        slivers: [
-          SliverPadding(
-            padding: const EdgeInsets.all(40),
-            sliver: SliverList(
-              delegate: SliverChildListDelegate([
-                Padding(
-                  padding: const EdgeInsets.only(top: 120),
-                  child: Center(
-                    child: Container(
-                      padding: const EdgeInsets.all(40),
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFFFF1AD),
-                        shape: BoxShape.circle,
-                      ),
-                      child: SizedBox(
-                        width: 180,
-                        height: 180,
-                        child: Image.asset(
-                          'assets/img/fruits.png',
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 36),
-                      child: Center(
-                        child: Text(
-                          "Fresh & Juicy",
-                          style: GoogleFonts.poppins(
-                            fontSize: 24,
-                            fontWeight: FontWeight.w600,
+      body: FutureBuilder(
+        future: getDataFromFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else {
+            return CustomScrollView(
+              slivers: [
+                SliverPadding(
+                  padding: const EdgeInsets.all(40),
+                  sliver: SliverList(
+                    delegate: SliverChildListDelegate([
+                      Padding(
+                        padding: const EdgeInsets.only(top: 120),
+                        child: Center(
+                          child: Container(
+                            padding: const EdgeInsets.all(40),
+                            decoration: const BoxDecoration(
+                              color: Color(0xFFFFF1AD),
+                              shape: BoxShape.circle,
+                            ),
+                            child: SizedBox(
+                              width: 180,
+                              height: 180,
+                              child: Image.asset(
+                                'assets/img/fruits.png',
+                                fit: BoxFit.fill,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Center(
-                        child: Text(
-                          "Get the fresh and juicy fruits at your doorstep.",
-                          style: GoogleFonts.poppins(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 36),
+                            child: Center(
+                              child: Text(
+                                "Fresh & Juicy",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
                           ),
-                          textAlign: TextAlign.center,
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Center(
+                              child: Text(
+                                "Get the fresh and juicy fruits at your doorstep.",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50),
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: () {
+                              double screenWidth =
+                                  MediaQuery.of(context).size.width;
+                              if (screenWidth >= 1080) {
+                                return 600.0;
+                              } else if (screenWidth >= 768) {
+                                return 200.0;
+                              } else if (screenWidth >= 640) {
+                                return 100.0;
+                              } else if (screenWidth >= 320) {
+                                return 20.0;
+                              } else {
+                                return 10.0;
+                              }
+                            }(),
+                          ),
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                context,
+                                '/product-list',
+                              );
+                            },
+                            style: TextButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 40,
+                                vertical: 20,
+                              ),
+                              backgroundColor: const Color(0xFFFFF1AD),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              'Shop Now',
+                              style: GoogleFonts.poppins(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 50),
-                  child: Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: () {
-                        double screenWidth = MediaQuery.of(context).size.width;
-                        if (screenWidth >= 1080) {
-                          return 600.0;
-                        } else if (screenWidth >= 768) {
-                          return 200.0;
-                        } else if (screenWidth >= 640) {
-                          return 100.0;
-                        } else if (screenWidth >= 320) {
-                          return 20.0;
-                        } else {
-                          return 10.0;
-                        }
-                      }(),
-                    ),
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          '/product-list',
-                        );
-                      },
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 40,
-                          vertical: 20,
-                        ),
-                        backgroundColor: const Color(0xFFFFF1AD),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: Text(
-                        'Shop Now',
-                        style: GoogleFonts.poppins(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
+                    ]),
                   ),
                 ),
-              ]),
-            ),
-          ),
-        ],
+              ],
+            );
+          }
+        },
       ),
     );
   }
